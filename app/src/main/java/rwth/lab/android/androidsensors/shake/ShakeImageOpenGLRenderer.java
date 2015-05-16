34121da5b3665b260d959859fad73a25dff79e2f
kate <lobanova.e.o@gmail.com>
2015-05-16 10:45:38 +0200

    Refactoring

--- app/src/main/java/rwth/lab/android/androidsensors/shake/ShakeImageOpenGLRenderer.java
+++ app/src/main/java/rwth/lab/android/androidsensors/shake/ShakeImageOpenGLRenderer.java
@@ -1,4 +1,4 @@
-﻿package rwth.lab.android.androidsensors.shake;
+package rwth.lab.android.androidsensors.shake;
 
 import android.content.Context;
 
@@ -10,10 +10,11 @@ import rwth.lab.android.androidsensors.sensor.IFigure;
 import rwth.lab.android.androidsensors.sensor.OpenGLRenderer;
 
 /**
- * Created by ekaterina on 15.05.2015.
+ * Created by ekaterina on 16.05.2015.
  */
 public class ShakeImageOpenGLRenderer extends OpenGLRenderer {
     private Context context;
+
     public ShakeImageOpenGLRenderer(Context context, IFigure figure) {
         super(context, figure);
         this.context = context;
@@ -24,14 +25,13 @@ public class ShakeImageOpenGLRenderer extends OpenGLRenderer {
         // Load the texture for the square
         figure.loadGLTexture(gl, this.context, R.drawable.android_shake);
 
-        gl.glEnable(GL10.GL_TEXTURE_2D);			//Enable Texture Mapping ( NEW )
-        gl.glShadeModel(GL10.GL_SMOOTH); 			//Enable Smooth Shading
-        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.5f); 	//Black Background
-        gl.glClearDepthf(1.0f); 					//Depth Buffer Setup
-        gl.glEnable(GL10.GL_DEPTH_TEST); 			//Enables Depth Testing
-        gl.glDepthFunc(GL10.GL_LEQUAL); 			//The Type Of Depth Testing To Do
+        gl.glEnable(GL10.GL_TEXTURE_2D);            //Enable Texture Mapping
+        gl.glShadeModel(GL10.GL_SMOOTH);            //Enable Smooth Shading
+        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.5f);    //Black Background
+        gl.glClearDepthf(1.0f);                    //Depth Buffer Setup
+        gl.glEnable(GL10.GL_DEPTH_TEST);            //Enables Depth Testing
+        gl.glDepthFunc(GL10.GL_LEQUAL);            //The Type Of Depth Testing To Do
 
-        //Really Nice Perspective Calculations
         gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);
     }
 
@@ -43,4 +43,3 @@ public class ShakeImageOpenGLRenderer extends OpenGLRenderer {
         super.onDrawFrame(gl);
     }
 }
-
